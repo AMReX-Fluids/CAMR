31969e543030c6cd52120be2d07da174d3c82eac
Weiqun Zhang <weiqunzhang@lbl.gov>
2024-01-22 13:27:02 -0800

    Prepare for amrex changes

--- Source/Utils/CAMR_setup.cpp
+++ Source/Utils/CAMR_setup.cpp
@@ -15,10 +15,10 @@ PassMap* CAMR::d_pass_map = nullptr;
 PassMap* CAMR::h_pass_map = nullptr;
 
 // Components are:
-//                          Interior, Inflow,  Outflow,  Symmetry,     SlipWall,      NoSlipWall
-static int scalar_bc[] =   {INT_DIR,  EXT_DIR, FOEXTRAP, REFLECT_EVEN, REFLECT_EVEN,  REFLECT_EVEN};
-static int norm_vel_bc[] = {INT_DIR,  EXT_DIR, FOEXTRAP, REFLECT_ODD , REFLECT_ODD,   REFLECT_ODD};
-static int tang_vel_bc[] = {INT_DIR,  EXT_DIR, FOEXTRAP, REFLECT_EVEN, REFLECT_EVEN,  REFLECT_ODD};
+//                          Interior,               Inflow,                 Outflow,                 Symmetry,                    SlipWall,                    NoSlipWall
+static int scalar_bc[] =   {amrex::BCType::int_dir, amrex::BCType::ext_dir, amrex::BCType::foextrap, amrex::BCType::reflect_even, amrex::BCType::reflect_even, amrex::BCType::reflect_even};
+static int norm_vel_bc[] = {amrex::BCType::int_dir, amrex::BCType::ext_dir, amrex::BCType::foextrap, amrex::BCType::reflect_odd , amrex::BCType::reflect_odd,  amrex::BCType::reflect_odd};
+static int tang_vel_bc[] = {amrex::BCType::int_dir, amrex::BCType::ext_dir, amrex::BCType::foextrap, amrex::BCType::reflect_even, amrex::BCType::reflect_even, amrex::BCType::reflect_odd};
 
 static amrex::Box
 the_same_box(const amrex::Box& b)
