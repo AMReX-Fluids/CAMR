eb9e5c2b2ac278cbf59bd012dbf6dbd02b420893
Ann Almgren <asalmgren@lbl.gov>
2026-08-22 14:32:32 -0700

    fixes issue 76 (#101)
    closes #76

--- Source/EB/eb_plane.cpp
+++ Source/EB/eb_plane.cpp
@@ -15,15 +15,9 @@ using namespace amrex;
 void make_eb_plane (const Geometry& geom, int max_coarsening_level, amrex::Real time)
 {
 
-    RealArray point;
-    point[0]=0.0 + 0.5*1.25e5*time*time;
-    point[1]=0.0;
-    point[2]=0.0;
+    RealArray point{AMREX_D_DECL(static_cast<Real>(0.0 + 0.5*1.25e5*time*time), 0.0, 0.0)};
 
-    RealArray normal;
-    normal[0]=-1.0;
-    normal[1]=0.0;
-    normal[2]=0.0;
+    RealArray normal{AMREX_D_DECL(-1.0, 0.0, 0.0)};
 
     EB2::PlaneIF pf(point, normal);
 
